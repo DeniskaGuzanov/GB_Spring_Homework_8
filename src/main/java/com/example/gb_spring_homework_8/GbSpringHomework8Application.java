d3fe71915047a25de7efa12ab82022b34e5b458a
denis <xdenis@yandex.ru>
2024-02-12 19:08:45 +0300

    Initial commit

---
+++ src/main/java/com/example/gb_spring_homework_8/GbSpringHomework8Application.java
@@ -0,0 +1,13 @@
+package com.example.gb_spring_homework_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSpringHomework8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbSpringHomework8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gb_spring_homework_8/GbSpringHomework8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gb_spring_homework_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringHomework8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
